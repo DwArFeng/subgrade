514f289f3db52d6ca95c302fb6b9b57e9b0adb6d
DwArFeng <915724865@qq.com>
2023-04-28 11:09:06 +0800

    优化 ServiceExceptionCodes.java 的代码格式
    Signed-off-by: DwArFeng <915724865@qq.com>

--- subgrade-sdk/src/main/java/com/dwarfeng/subgrade/sdk/exception/ServiceExceptionCodes.java
+++ subgrade-sdk/src/main/java/com/dwarfeng/subgrade/sdk/exception/ServiceExceptionCodes.java
@@ -17,6 +17,7 @@ public final class ServiceExceptionCodes {
      */
     public static final ServiceException.Code KEY_FETCH_FAILED =
             new ServiceException.Code(offset(10), "guid fetch failed");
+
     /**
      * 缓存异常。
      */
@@ -28,46 +29,55 @@ public final class ServiceExceptionCodes {
      */
     public static final ServiceException.Code UNDEFINE =
             new ServiceException.Code(offset(1), "undefine");
+
     /**
      * 数据访问层异常。
      */
     public static final ServiceException.Code DAO_FAILED =
             new ServiceException.Code(offset(30), "dao failed");
+
     /**
      * 实体已经存在。
      */
     public static final ServiceException.Code ENTITY_EXISTED =
             new ServiceException.Code(offset(31), "entity existed");
+
     /**
      * 实体不存在。
      */
     public static final ServiceException.Code ENTITY_NOT_EXIST =
             new ServiceException.Code(offset(32), "entity not existed");
+
     /**
      * 参数验证失败。
      */
     public static final ServiceException.Code PARAM_VALIDATION_FAILED =
             new ServiceException.Code(offset(40), "param validation failed");
+
     /**
      * IO异常。
      */
     public static final ServiceException.Code IO_EXCEPTION =
             new ServiceException.Code(offset(50), "io exception");
+
     /**
      * 过程异常。
      */
     public static final ServiceException.Code PROCESS_FAILED =
             new ServiceException.Code(offset(60), "process failed");
+
     /**
      * 处理器异常。
      */
     public static final ServiceException.Code HANDLER_FAILED =
             new ServiceException.Code(offset(70), "handler failed");
+
     /**
      * 权限拒绝。
      */
     public static final ServiceException.Code PERMISSION_DENIED =
             new ServiceException.Code(offset(80), "permission denied");
+
     /**
      * 登录失败。
      */
